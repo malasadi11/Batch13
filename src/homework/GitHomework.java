a99403637a4fb19a2536000678ea4b847f78ad36
munaa <munaa@HP.lan>
2024-09-10 21:15:28 -0400

    commit

--- src/homework/GitHomework.java
+++ src/homework/GitHomework.java
@@ -1,32 +1,5 @@
 package homework;
 
 public class GitHomework {
-	public static void main(String[] args) {
-		int age = 12;
-		String name = "John";
-		// this is coming from GitHub
-		
-		
-	}
-		
-		public static void add( int a, int b) {
-			int a = 0;
-			int b = 3;
-			int c = 4; 
-			System.out.println(a+b);
-		}
-		static void muna( char a) {
-			char a ='a'; 
-			char b = 'b'
-			char c = 'c'
-				// this is coming from GitHub
-			System.out.println(a);
-		}
-
-	public void main( int a) {
-		
-		System.out.println(a);
-	}
-	}
-
+	
 
